f08e6ed9bdd0d37b626ba2b9f4b3fb74f92307e7
Bruno Felipe <brunof.bem@gmail.com>
2018-05-11 15:25:58 -0300

    Added util class Message to represent a message object for the responses

---
+++ src/main/java/br/com/blz/testjava/util/Message.java
@@ -0,0 +1,19 @@
+package br.com.blz.testjava.util;
+
+public class Message {
+
+	private String message;
+
+	public Message(String message) {
+		this.setMessage(message);
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}	
+	
+}
