03763119763da765814e6ef119d82658d6147d60
Diego Rocha <diegopolovinsky@gmail.com>
2018-05-14 23:00:11 -0300

    Implemented findAll and findBySku

--- src/main/java/br/com/blz/testjava/inventory/InventoryService.java
+++ src/main/java/br/com/blz/testjava/inventory/InventoryService.java
@@ -3,11 +3,17 @@ package br.com.blz.testjava.inventory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InventoryService {
 
     @Autowired
     private InventoryRepository repository;
+
+    public List<Inventory> findAll() {
+        return repository.findAll();
+    }
 
     public Inventory findByProduct(Long product) {
         return repository.findOne(product);
--- src/test/java/br/com/blz/testjava/inventory/InventoryServiceTest.java
+++ src/test/java/br/com/blz/testjava/inventory/InventoryServiceTest.java
@@ -1,13 +1,16 @@
 package br.com.blz.testjava.inventory;
 
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,16 +21,29 @@ public class InventoryServiceTest {
 
     @Before
     public void setUp() throws Exception {
-        Inventory inventory = new Inventory();
-        inventory.setName("jajaj");
-        inventory.setSku(1L);
-        service.save(inventory);
+        Inventory inventory1 = new Inventory();
+        inventory1.setName("L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium - Máscara de Reconstrução 500g");
+        inventory1.setSku(1L);
+
+        Inventory inventory2 = new Inventory();
+        inventory2.setName("Kérastase Spécifique Bain Vital Dermo-Calm Edição Limitada - Shampoo 500ml");
+        inventory2.setSku(2L);
+
+        service.save(inventory1);
+        service.save(inventory2);
     }
 
     @Test
-    public void findProduct_xxx_xxx() {
+    public void findProductAll_shouldReturnAllInventories() {
+        List<Inventory> inventory = service.findAll();
+        assertThat(inventory, Matchers.hasSize(2));
+    }
+
+    @Test
+    public void findProductBySku_shouldReturnInventory() {
         Inventory inventory = service.findByProduct(1L);
-        Assert.assertThat(inventory, Matchers.notNullValue());
-        Assert.assertThat(inventory.getName(), Matchers.equalTo("jajaj"));
+        assertThat(inventory, Matchers.notNullValue());
+        assertThat(inventory.getName(),
+                Matchers.equalTo("L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium - Máscara de Reconstrução 500g"));
     }
 }
