cd9d31441ac407d4b4723cb54a620d7bb5b9ab9f
Diego Rocha <diegopolovinsky@gmail.com>
2018-05-15 00:01:03 -0300

    refactoring

--- src/main/java/br/com/blz/testjava/inventory/Inventory.java
+++ src/main/java/br/com/blz/testjava/inventory/Inventory.java
@@ -2,7 +2,11 @@ package br.com.blz.testjava.inventory;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+
+import static br.com.blz.testjava.inventory.Type.ECOMMERCE;
+import static br.com.blz.testjava.inventory.Type.PHYSICAL_STORE;
 
 @Entity
 public class Inventory implements Serializable {
@@ -15,6 +19,22 @@ public class Inventory implements Serializable {
     private String name;
     @Transient
     private List<Warehouse> warehouses;
+
+    public Inventory() {
+        this.warehouses = new ArrayList<>();
+
+        Warehouse warehouse1 = new Warehouse();
+        warehouse1.setLocality("SP");
+        warehouse1.setQuantity(12);
+        warehouse1.setTypes(ECOMMERCE);
+        warehouses.add(warehouse1);
+
+        Warehouse warehouse2 = new Warehouse();
+        warehouse2.setLocality("MOEMA");
+        warehouse2.setQuantity(3);
+        warehouse2.setTypes(PHYSICAL_STORE);
+        warehouses.add(warehouse2);
+    }
 
     public Long getId() {
         return id;
--- src/main/java/br/com/blz/testjava/inventory/Warehouse.java
+++ src/main/java/br/com/blz/testjava/inventory/Warehouse.java
@@ -5,8 +5,31 @@ import java.io.Serializable;
 public class Warehouse implements Serializable {
 
     private Long id;
-    private Long sku;
     private String locality;
     private Integer quantity;
     private Enum<Type> types;
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Enum<Type> getTypes() {
+        return types;
+    }
+
+    public void setTypes(Enum<Type> types) {
+        this.types = types;
+    }
 }
