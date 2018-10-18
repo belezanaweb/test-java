928153225ae240248174979cc8ace2ae00216aef
Hermes Gonzalez <hermesinfo@yahoo.com.br>
2018-10-18 00:49:46 -0300

    teste

--- src/main/java/br/com/blz/testjava/TestJavaApplication.java
+++ src/main/java/br/com/blz/testjava/TestJavaApplication.java
@@ -8,5 +8,7 @@ public class TestJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestJavaApplication.class, args);
+		
+		System.out.println("teste commit");
 	}
 }
