0a08d7c8b34210631cbec417679a56752cc694a1
Rafael Cheruti <rcheruti@gmail.com>
2018-11-27 15:49:58 -0200

    Add logs na borda

--- src/main/java/br/com/blz/testjava/bordas/ProdutoBorda.java
+++ src/main/java/br/com/blz/testjava/bordas/ProdutoBorda.java
@@ -41,16 +41,21 @@ public class ProdutoBorda {
   @PostMapping("/produtos")
   public void criar(@RequestBody Produto produto){
     produtoControle.gravar(produto);
+    log.info("Criado o Produto de ID/SKU: {}", produto.getSku());
   }
   
   @PutMapping("/produtos")
   public void atualizar(@RequestBody Produto produto){
+    if( produto.getSku() == 0 ) throw new MsgException("Deve ser informado o ID/SKU do produto que será atualizado!", null);
     produtoControle.atualizar(produto);
+    log.info("Atualizado o Produto de ID/SKU: {}", produto.getSku());
   }
   
   @DeleteMapping("/produtos/{id}")
   public void deletar(@PathVariable("id") int id){
+    if( id == 0 ) throw new MsgException("Deve ser informado o ID/SKU do produto que será deletado!", null);
     produtoControle.deletar(id);
+    log.info("Deletado o Produto de ID/SKU: {}", id);
   }
   
 } 
