8387f751f8caff744662d8737fb5b187db24f014
alfaville <lipealfaville@gmail.com>
2019-02-21 20:15:26 -0300

    Refatoração e melhoria

---
+++ src/main/java/br/com/blz/testjava/controllers/dto/response/ResponseDto.java
@@ -0,0 +1,12 @@
+package br.com.blz.testjava.controllers.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ResponseDto {
+
+    private Long sku;
+
+}
---
+++ src/main/java/br/com/blz/testjava/controllers/exception/ApiError.java
@@ -0,0 +1,34 @@
+package br.com.blz.testjava.controllers.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+public class ApiError {
+
+    private int status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private Date timestamp;
+
+    private String message;
+
+    private ApiError() {
+        this.timestamp = new Date();
+    }
+
+    public ApiError(int status) {
+        this();
+        this.status = status;
+    }
+
+    public ApiError(int status, String message, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = message;
+    }
+
+}
