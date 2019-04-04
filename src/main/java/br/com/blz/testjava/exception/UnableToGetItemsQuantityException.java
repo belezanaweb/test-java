d4bdd45ec18c4183a1fd099d6371c529fc547172
Rafael Rosa <rafaelfrancorosa@hotmail.com>
2019-04-04 20:09:24 -0300

    Altering serialVersionUID of exception

--- src/main/java/br/com/blz/testjava/exception/UnableToGetItemsQuantityException.java
+++ src/main/java/br/com/blz/testjava/exception/UnableToGetItemsQuantityException.java
@@ -1,8 +1,8 @@
 package br.com.blz.testjava.exception;
 
 public class UnableToGetItemsQuantityException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
+	
+	private static final long serialVersionUID = 5257086606615985143L;
 
 	public UnableToGetItemsQuantityException(String message) {
 		super(message);
