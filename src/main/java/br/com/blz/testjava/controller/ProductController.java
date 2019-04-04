90dcc3e0a1e9af5fc72eff2b35d2ea6b4bbb40da
Rafael Rosa <rafaelfrancorosa@hotmail.com>
2019-04-04 15:25:27 -0300

    endpoint to get all skus (tests)

--- src/main/java/br/com/blz/testjava/controller/ProductController.java
+++ src/main/java/br/com/blz/testjava/controller/ProductController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,10 @@ public class ProductController implements ProductApi {
 	
 	public ResponseEntity deleteProduct(@PathVariable("sku") Long sku) {
 		return new ResponseEntity<>(service.deleteProduct(sku), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/product/testGetAll", produces = { "application/json" })
+	public ResponseEntity getAllSkus() {
+		return new ResponseEntity<>(service.testGetAll(), HttpStatus.OK);
 	}
 }
