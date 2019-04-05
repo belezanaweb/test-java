204d8eca8ae036acadca8d9cb97fd1f4ce6236b6
Rafael Rosa <rafaelfrancorosa@hotmail.com>
2019-04-04 22:30:59 -0300

    Altering returns in post, put, delete

--- src/main/java/br/com/blz/testjava/controller/ProductController.java
+++ src/main/java/br/com/blz/testjava/controller/ProductController.java
@@ -12,17 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.blz.testjava.api.ProductApi;
 import br.com.blz.testjava.model.Product;
+import br.com.blz.testjava.model.ResponseSuccess;
 import br.com.blz.testjava.service.ProductService;
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
 @RestController
 public class ProductController implements ProductApi {
 	
+	private static final String SUCESS_PREFIX_MSG = "Product with id ";
+	
 	@Autowired
 	ProductService service;
 	
 	public ResponseEntity createProduct(@Valid @RequestBody Product body) {
-		return new ResponseEntity<>(service.postProduct(body), HttpStatus.CREATED); 
+		Product createdProduct = service.postProduct(body);
+		
+		return new ResponseEntity<>(
+			new ResponseSuccess().message(SUCESS_PREFIX_MSG + createdProduct.getSku()+" was created")
+		,	HttpStatus.CREATED); 
 	}
 	
 	public ResponseEntity getProductBySKU(@PathVariable("sku") Long sku) {
@@ -31,11 +38,18 @@ public class ProductController implements ProductApi {
 	
 	public ResponseEntity updateProduct(@Valid @RequestBody Product body) {
 		Product updatedItem = service.updateProduct(body);
-		return new ResponseEntity<>("Product with id "+updatedItem.getSku()+" was updated", HttpStatus.OK);
+		
+		return new ResponseEntity<>(
+			new ResponseSuccess().message(SUCESS_PREFIX_MSG + updatedItem.getSku()+" was updated")
+		,	HttpStatus.OK);
 	}
 	
 	public ResponseEntity deleteProduct(@PathVariable("sku") Long sku) {
-		return new ResponseEntity<>(service.deleteProduct(sku), HttpStatus.OK);
+		Product deletedItem = service.deleteProduct(sku);
+		
+		return new ResponseEntity<>(
+			new ResponseSuccess().message(SUCESS_PREFIX_MSG + deletedItem.getSku()+" was deleted")
+		,	HttpStatus.OK);
 	}
 	
 	@GetMapping(value = "/product/testGetAll", produces = { "application/json" })
