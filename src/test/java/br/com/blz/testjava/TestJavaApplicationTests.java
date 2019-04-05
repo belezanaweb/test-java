7ff938e5e8c837a97b0ccb2fc1550f0a866943dc
Rafael Rosa <rafaelfrancorosa@hotmail.com>
2019-04-05 00:09:34 -0300

    Including random port in boot test

--- src/test/java/br/com/blz/testjava/TestJavaApplicationTests.java
+++ src/test/java/br/com/blz/testjava/TestJavaApplicationTests.java
@@ -3,10 +3,11 @@ package br.com.blz.testjava;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TestJavaApplicationTests {
 
 	@Test
