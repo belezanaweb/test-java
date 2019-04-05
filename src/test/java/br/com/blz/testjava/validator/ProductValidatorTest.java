714abfb2aad79ca8f8003f229c5022624c8bc172
Rafael Rosa <rafaelfrancorosa@hotmail.com>
2019-04-05 03:50:19 -0300

    New tests after comparator implement

--- src/test/java/br/com/blz/testjava/validator/ProductValidatorTest.java
+++ src/test/java/br/com/blz/testjava/validator/ProductValidatorTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import br.com.blz.testjava.exception.DupItemsInWarehousesException;
 import br.com.blz.testjava.exception.InvalidProductNameException;
 import br.com.blz.testjava.exception.InvalidQuantityInventoryLinkException;
 import br.com.blz.testjava.exception.InvalidTotalProductQuantityException;
@@ -20,6 +21,7 @@ import br.com.blz.testjava.model.Warehouse;
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public class ProductValidatorTest {
 	
+	private static final String WARE_KIND_2 = "PHYSICAL_STORE";
 	private static final String WARE_KIND = "ECOMMERCE";
 	private static final String FIRST_WARE_LOCATION = "SP";
 	private static final Boolean NOT_MARKETABLE = Boolean.FALSE;
@@ -86,7 +88,7 @@ public class ProductValidatorTest {
 		Warehouse ware = new Warehouse();
 		ware.setQuantity(0L);
 		ware.setLocality(FIRST_WARE_LOCATION);
-		ware.setKind(WARE_KIND);
+		ware.setType(WARE_KIND);
 		warehousesList.add(ware);
 		inventory.setWarehouses(warehousesList);
 		
@@ -101,7 +103,7 @@ public class ProductValidatorTest {
 		Warehouse warehouse = warehousesList.get(0);
 		assertEquals(Long.valueOf(0), warehouse.getQuantity());
 		assertEquals(FIRST_WARE_LOCATION, warehouse.getLocality());
-		assertEquals(WARE_KIND, warehouse.getKind());
+		assertEquals(WARE_KIND, warehouse.getType());
 	}
 	
 	@Test
@@ -117,7 +119,7 @@ public class ProductValidatorTest {
 		Warehouse ware = new Warehouse();
 		ware.setQuantity(9L);
 		ware.setLocality(FIRST_WARE_LOCATION);
-		ware.setKind(WARE_KIND);
+		ware.setType(WARE_KIND);
 		warehousesList.add(ware);
 		inventory.setWarehouses(warehousesList);
 		
@@ -132,7 +134,7 @@ public class ProductValidatorTest {
 		Warehouse warehouse = warehousesList.get(0);
 		assertEquals(Long.valueOf(9), warehouse.getQuantity());
 		assertEquals(FIRST_WARE_LOCATION, warehouse.getLocality());
-		assertEquals(WARE_KIND, warehouse.getKind());
+		assertEquals(WARE_KIND, warehouse.getType());
 	}
 	
 	@Test(expected=InvalidQuantityInventoryLinkException.class)
@@ -147,8 +149,36 @@ public class ProductValidatorTest {
 		Warehouse ware = new Warehouse();
 		ware.setQuantity(9L);
 		ware.setLocality(FIRST_WARE_LOCATION);
-		ware.setKind(WARE_KIND);
+		ware.setType(WARE_KIND);
 		warehousesList.add(ware);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test(expected=DupItemsInWarehousesException.class)
+	public void tryValidateProductWithInventoryContainingSameWarehousesAndFail() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(0L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setQuantity(9L);
+		ware.setLocality(FIRST_WARE_LOCATION);
+		ware.setType(WARE_KIND);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(-9L);
+		ware2.setLocality(FIRST_WARE_LOCATION);
+		ware2.setType(WARE_KIND);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
 		inventory.setWarehouses(warehousesList);
 		
 		prod.setInventory(inventory);
@@ -168,12 +198,235 @@ public class ProductValidatorTest {
 		Warehouse ware = new Warehouse();
 		ware.setQuantity(9L);
 		ware.setLocality(FIRST_WARE_LOCATION);
-		ware.setKind(WARE_KIND);
+		ware.setType(WARE_KIND);
 		
 		Warehouse ware2 = new Warehouse();
 		ware2.setQuantity(-9L);
 		ware2.setLocality(FIRST_WARE_LOCATION);
-		ware2.setKind(WARE_KIND);
+		ware2.setType(WARE_KIND_2);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test
+	public void tryValidateProductWithInventoryWithItemsInWarehouses() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(18L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setQuantity(9L);
+		ware.setLocality(FIRST_WARE_LOCATION);
+		ware.setType(WARE_KIND);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(9L);
+		ware2.setLocality(FIRST_WARE_LOCATION);
+		ware2.setType(WARE_KIND_2);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test
+	public void tryValidateProductWithInventoryWithItemsInWarehousesNullLocation1() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(18L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setQuantity(9L);
+		ware.setLocality(null);
+		ware.setType(WARE_KIND);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(9L);
+		ware2.setLocality(FIRST_WARE_LOCATION);
+		ware2.setType(WARE_KIND_2);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test
+	public void tryValidateProductWithInventoryWithItemsInWarehousesNullLocation2() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(18L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setQuantity(9L);
+		ware.setLocality(FIRST_WARE_LOCATION);
+		ware.setType(WARE_KIND);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(9L);
+		ware2.setLocality(null);
+		ware2.setType(WARE_KIND_2);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test
+	public void tryValidateProductWithInventoryWithItemsInWarehousesNullType1() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(18L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setQuantity(9L);
+		ware.setLocality(FIRST_WARE_LOCATION);
+		ware.setType(null);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(9L);
+		ware2.setLocality(FIRST_WARE_LOCATION);
+		ware2.setType(WARE_KIND_2);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test
+	public void tryValidateProductWithInventoryWithItemsInWarehousesNullType2() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(18L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setQuantity(9L);
+		ware.setLocality(FIRST_WARE_LOCATION);
+		ware.setType(WARE_KIND);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(9L);
+		ware2.setLocality(FIRST_WARE_LOCATION);
+		ware2.setType(null);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test(expected=DupItemsInWarehousesException.class)
+	public void tryValidateProductWithInventoryWithItemsInWarehousesNullFields() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(18L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setQuantity(9L);
+		ware.setLocality(null);
+		ware.setType(null);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(9L);
+		ware2.setLocality(null);
+		ware2.setType(null);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test
+	public void tryValidateProductWithInventoryWithItemsInWarehousesNullQuantityFirstWarehouse() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(9L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setLocality(FIRST_WARE_LOCATION);
+		ware.setType(WARE_KIND);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(9L);
+		ware2.setLocality(FIRST_WARE_LOCATION);
+		ware2.setType(null);
+		
+		warehousesList.add(ware);
+		warehousesList.add(ware2);
+		inventory.setWarehouses(warehousesList);
+		
+		prod.setInventory(inventory);
+		ProductValidator.validate(prod);
+	}
+	
+	@Test(expected=InvalidQuantityInventoryLinkException.class)
+	public void tryValidateProductWithInventoryWithItemsInWarehousesNegativeQuantityFirstWarehouse() {
+		Product prod = new Product();
+		prod.setName(PROD_NAME);
+		prod.setIsMarketable(NOT_MARKETABLE);
+		
+		Inventory inventory = new Inventory();
+		inventory.setQuantity(8L);
+		
+		List<Warehouse> warehousesList = new ArrayList();
+		Warehouse ware = new Warehouse();
+		ware.setQuantity(-1L);
+		ware.setLocality(FIRST_WARE_LOCATION);
+		ware.setType(WARE_KIND);
+		
+		Warehouse ware2 = new Warehouse();
+		ware2.setQuantity(9L);
+		ware2.setLocality(FIRST_WARE_LOCATION);
+		ware2.setType(null);
 		
 		warehousesList.add(ware);
 		warehousesList.add(ware2);
