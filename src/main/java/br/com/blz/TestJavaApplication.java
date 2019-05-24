4e00d333a28ae79ca501fa6a579c830009142d92
rzacanti <rzacanti@gmail.com>
2019-05-24 10:47:27 -0300

    Primeiro commit

--- src/main/java/br/com/blz/TestJavaApplication.java
+++ src/main/java/br/com/blz/TestJavaApplication.java
@@ -1,12 +1,12 @@
-package br.com.blz.testjava;
+package br.com.blz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackageClasses = TestJavaApplication.class)
+@SpringBootApplication
 public class TestJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestJavaApplication.class, args);
-	}
+		}
 }
