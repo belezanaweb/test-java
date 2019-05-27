ce2afb476c57090a0ee75e7a447acf4fb89af904
Bruno Fernandes <bruno.reis@gsw.com.br>
2019-05-27 15:18:02 -0300

    Retirada de import nao utilizado

--- src/main/java/br/com/blz/testjava/models/Inventory.java
+++ src/main/java/br/com/blz/testjava/models/Inventory.java
@@ -3,7 +3,6 @@ package br.com.blz.testjava.models;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
