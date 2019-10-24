8f6cf52b87fa597a7d8d14d6af4112c3f43a7d61
Euler Santana <euler.santana@yahoo.com>
2019-10-24 01:35:42 -0300

    Primeira versao

--- src/main/java/br/com/blz/testjava/TestJavaApplication.java
+++ src/main/java/br/com/blz/testjava/TestJavaApplication.java
@@ -3,7 +3,10 @@ package br.com.blz.testjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication(scanBasePackageClasses = TestJavaApplication.class)
+@EnableSwagger2
 public class TestJavaApplication {
 
 	public static void main(String[] args) {
