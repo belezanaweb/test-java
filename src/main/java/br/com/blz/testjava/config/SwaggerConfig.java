d3564b19fd71d9da2a537585466eac799b3fe788
jonatas <jonatas.system@gmail.com>
2019-11-04 11:07:25 -0300

    ajustes no config swagger

--- src/main/java/br/com/blz/testjava/config/SwaggerConfig.java
+++ src/main/java/br/com/blz/testjava/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiEndPointsInfo() {
-		return new ApiInfoBuilder().title("Spring Boot REST API").description("Employee Management REST API")
+		return new ApiInfoBuilder().title("Spring Boot REST API").description("Product Management REST API")
 				.contact(new Contact("Jônatas Rodrigues", "https://www.linkedin.com/in/j%C3%B4natas-rodrigues-6620455a/",
 						"jonatas.system@gmail.com")).build();
 	}
