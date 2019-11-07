dd7b1b38f75accfbcfe8ecb754d6dbc6787a2fd8
Bruno Saar <bsaar.verx@rd.com.br>
2019-11-07 10:03:40 -0300

    Realizando alguns ajustes e melhorias

--- src/main/java/br/com/blz/testjava/repository/ProductRepository.java
+++ src/main/java/br/com/blz/testjava/repository/ProductRepository.java
@@ -5,25 +5,26 @@ import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class ProductRepository {
 
-    private Map<Long, Product> products = new HashMap<Long, Product>();
+    private Map<Long, Product> products = new HashMap<>();
 
-    public void addProduct(Product product) {
+    public void save(Product product) {
         products.put(product.getSku(), product);
     }
 
-    public Product findProductBySku(Long sku) {
-        return products.get(sku);
+    public Optional<Product> findById(Long sku) {
+        return Optional.ofNullable(products.get(sku));
     }
 
-    public void deleteProduct(Long sku) {
+    public void deleteById(Long sku) {
         products.remove(sku);
     }
 
-    public void updateProduct(Long sku, Product product) {
+    public void update(Long sku, Product product) {
         products.replace(sku, product);
     }
 }
--- src/main/java/br/com/blz/testjava/service/ProductService.java
+++ src/main/java/br/com/blz/testjava/service/ProductService.java
@@ -7,60 +7,46 @@ import br.com.blz.testjava.exception.ProductNotFoundException;
 import br.com.blz.testjava.exception.ProductSkuImmutableException;
 import br.com.blz.testjava.repository.ProductRepository;
 import br.com.blz.testjava.service.bo.ProductBO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
 
     public Product findProductBySku(long sku){
-        Product product = productRepository.findProductBySku(sku);
-
-        if(product == null) throw new ProductNotFoundException();
-
+        Product product = productRepository.findById(sku).orElseThrow(()->new ProductNotFoundException());
         product.getInventory().setQuantity(0);
-
         product.getInventory().getWarehouses().stream().forEach(
             f-> product.getInventory().setQuantity(
                 product.getInventory().getQuantity() + f.getQuantity()
             )
         );
-
         product.setIsMarketable(product.getInventory().getQuantity() > 0);
-
         return product;
     }
 
     public void deleteProduct(Long sku) {
-
-        Product product = productRepository.findProductBySku(sku);
-
-        if(product == null) throw new ProductNotFoundException();
-
-        productRepository.deleteProduct(sku);
+        productRepository.findById(sku).orElseThrow(() ->new ProductNotFoundException());
+        productRepository.deleteById(sku);
     }
 
     public void createProduct(ProductAttributes productAttributes) {
         Product product = ProductBO.parsePojoToEntity(productAttributes);
-
-        if(productRepository.findProductBySku(product.getSku()) != null)
-            throw new ProductExistsException();
-
-        productRepository.addProduct(product);
+        productRepository.findById(product.getSku()).ifPresent(function -> {throw new ProductExistsException();});
+        productRepository.save(product);
     }
 
     public void update(Long sku, ProductAttributes productAttributes) {
         Product product = ProductBO.parsePojoToEntity(productAttributes);
 
-        if(productRepository.findProductBySku(sku) == null)
-            throw new ProductNotFoundException();
+        productRepository.findById(sku).orElseThrow(() ->new ProductNotFoundException());
+        if(sku.compareTo(product.getSku()) != 0) throw new ProductSkuImmutableException();
 
-        if(sku.compareTo(product.getSku()) != 0)
-            throw new ProductSkuImmutableException();
-
-        productRepository.updateProduct(sku, product);
+        productRepository.update(sku, product);
     }
 }
