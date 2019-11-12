759796c070cd76a8e7e4a5980d01c29a39a1cbd4
Luiz Evangelista <luizhenrique.se@gmail.com>
2019-11-12 09:59:44 -0300

    criando calculo de quantity do inventory e regra do marketable

---
+++ src/main/java/br/com/blz/testjava/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package br.com.blz.testjava.repository;
+
+import br.com.blz.testjava.domain.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ProductRepository {
+
+    private Map<Long, Product> productMap = new HashMap<>();
+
+    public Product save(Product product) {
+        productMap.put(product.getSku(), product);
+        return product;
+    }
+}
--- src/main/java/br/com/blz/testjava/service/impl/ProductServiceImpl.java
+++ src/main/java/br/com/blz/testjava/service/impl/ProductServiceImpl.java
@@ -1,14 +1,41 @@
 package br.com.blz.testjava.service.impl;
 
+import br.com.blz.testjava.domain.Inventory;
 import br.com.blz.testjava.domain.Product;
+import br.com.blz.testjava.domain.Warehouse;
+import br.com.blz.testjava.repository.ProductRepository;
 import br.com.blz.testjava.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+
+    private final ProductRepository repository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Product create(Product product) {
-        return product;
+        Inventory inventory = new Inventory.Builder()
+            .withWarehouses(product.getInventory().getWarehouses())
+            .withQuantity(calculateInventoryQuantity(product.getInventory().getWarehouses()))
+            .build();
+        product.setInventory(inventory);
+        product.setMarketable(inventory.getQuantity() > 0 ? true : false);
+        return repository.save(product);
+    }
+
+    private Integer calculateInventoryQuantity(List<Warehouse> warehouses) {
+        return warehouses.stream()
+            .map(w -> w.getQuantity())
+            .collect(Collectors.summingInt(Integer::intValue));
     }
 }
