17ccac0876bc6b06c696f5cbccb704c960805f75
Fernando Costa Leite <clfernando1979@hotmail.com>
2021-01-11 19:57:27 -0300

    criando camadas service e controller

---
+++ src/main/java/br/com/blz/testjava/controller/ProdutoController.java
@@ -0,0 +1,42 @@
+package br.com.blz.testjava.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.blz.testjava.dto.ProdutoSalvamentoDTO;
+import br.com.blz.testjava.service.ProdutoService;
+
+
+@RestController
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoService service;
+
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody ProdutoSalvamentoDTO produto) {
+
+		return new ResponseEntity<>(service.save(produto), HttpStatus.CREATED);
+
+	}
+
+	@GetMapping("/{sku}")
+	public ResponseEntity<?> findBySku(@PathVariable Long sku) {
+
+		return new ResponseEntity<>(service.findBySku(sku), HttpStatus.OK);
+	}
+
+	@PutMapping("/{sku}")
+	public ResponseEntity<?> delete(@PathVariable Long sku) {
+		service.delete(sku);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+}
---
+++ src/main/java/br/com/blz/testjava/service/ProdutoService.java
@@ -0,0 +1,35 @@
+package br.com.blz.testjava.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.blz.testjava.dto.ProdutoDTO;
+import br.com.blz.testjava.dto.ProdutoSalvamentoDTO;
+import br.com.blz.testjava.mapper.ProdutoMapper;
+import br.com.blz.testjava.model.Produto;
+import br.com.blz.testjava.repository.IProdutoRepository;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private IProdutoRepository produtoRepository;
+	
+	public ProdutoDTO save(ProdutoSalvamentoDTO produtoDTO) {
+		
+		Produto produto = ProdutoMapper.toEntity(produtoDTO);
+		produtoRepository.save(produto);
+		
+		return ProdutoMapper.toProdutoDTO(produto);
+	}
+	public ProdutoDTO findBySku(Long sku) {
+		Produto produto = produtoRepository.findBySku(sku).orElseThrow(() -> new RuntimeException());
+		
+		return null;
+	}
+	public Object delete(Long sku) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
