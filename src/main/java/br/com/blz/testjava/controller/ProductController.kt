2995d35ce2a9ac9fcf873629ab5871201bab0184
mauriciomarqs <mauriciojmarx@gmail>
2021-05-31 15:52:02 -0300

    Commit inicial e final, havia feito boa parte em java mas li que kotlin seria bem vindo entao mudei

--- src/main/java/br/com/blz/testjava/controller/ProductController.kt
+++ src/main/java/br/com/blz/testjava/controller/ProductController.kt
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("product")
 class ProductController {
 
     @Autowired lateinit var productService: ProductService
