06ca649b7920e3f1f6647400d1df92214b6ad9fe
Diogenes Rabelo <diogenes.rabelo@contabilizei.com.br>
2021-08-02 16:19:18 -0300

    Add test application

---
+++ src/test/java/br/com/blz/testjava/TestJavaApplicationTest.java
@@ -0,0 +1,15 @@
+package br.com.blz.testjava;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class TestJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
