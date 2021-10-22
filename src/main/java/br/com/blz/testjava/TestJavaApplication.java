cf6196ce3c71a99ab5db78a2b4be6edd02891114
Thiago <thiagolenz@gmail.com>
2021-10-22 16:44:10 -0300

    Parte 6 - correcoes do teste unitario do maven
    não estava executando os testes unitarios

--- src/main/java/br/com/blz/testjava/TestJavaApplication.java
+++ src/main/java/br/com/blz/testjava/TestJavaApplication.java
@@ -1,7 +1,11 @@
 package br.com.blz.testjava;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.kotlin.KotlinModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackageClasses = TestJavaApplication.class)
 public class TestJavaApplication {
@@ -10,4 +14,15 @@ public class TestJavaApplication {
 		SpringApplication.run(TestJavaApplication.class, args);
 	}
 
+    /**
+     * Coloquei essa configuração para o Jackson que é usado no spring fazer a conversao
+     * de classes do Kotlin
+     */
+    @Bean
+    public ObjectMapper objectMapper () {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new KotlinModule());
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
 }
