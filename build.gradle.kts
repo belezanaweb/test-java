f5d07780d13bf03c411861164697734553f6668a
Andre Rocha <andre.rocha@maxmilhas.com.br>
2022-03-18 11:36:00 -0300

    refact: convert project to kotlin and gradle

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "br.com.blz"
+version = "0.0.1-SNAPSHOT"
+
+extra["springBootVersion"] = "2.6.4"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
+plugins {
+    id("org.springframework.boot") version "2.6.4"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.21"
+    kotlin("plugin.spring") version "1.5.21"
+    kotlin("plugin.jpa") version "1.5.21"
+    kotlin("plugin.allopen") version "1.5.21"
+    kotlin("kapt") version "1.5.21"
+}
+
+repositories {
+	  mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:${property("springBootVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:${property("springBootVersion")}")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/br/com/blz/testjava/TestJavaApplicationTests.kt
@@ -0,0 +1,14 @@
+package br.com.blz.testjava
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.boot.test.context.SpringBootTest
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TestJavaApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
