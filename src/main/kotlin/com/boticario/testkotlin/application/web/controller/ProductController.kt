fefa6843a9b6cafba5a6ff038591dc184cd34361
gabrielmaximo <familia_maximo@hotmail.com>
2023-02-21 19:26:48 -0300

    add transaction to update resource

--- src/main/kotlin/com/boticario/testkotlin/application/web/controller/ProductController.kt
+++ src/main/kotlin/com/boticario/testkotlin/application/web/controller/ProductController.kt
@@ -3,7 +3,9 @@ package com.boticario.testkotlin.application.web.controller
 import com.boticario.testkotlin.application.request.CreateProductRequest
 import com.boticario.testkotlin.application.request.UpdateProductRequest
 import com.boticario.testkotlin.application.service.ProductService
+import java.sql.SQLException
 import org.springframework.http.HttpStatus
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 @RequestMapping("/products")
@@ -26,6 +29,7 @@ class ProductController(private val productService: ProductService) {
     fun getProduct(@PathVariable sku: Long) = productService.getProductFromStock(sku)
 
     @PutMapping("/{sku}")
+    @Transactional(rollbackFor = [SQLException::class])
     fun updateProduct(@RequestBody updateProductRequest: UpdateProductRequest, @PathVariable sku: Long) =
             productService.updateProductFromStock(updateProductRequest, sku)
 
